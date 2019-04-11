c49b156a02b831fec12187bcc017fbe85aa485fa
LukeDSchenk <lschenk@next.tech>
2019-04-11 15:47:38 -0400

    Final test commit for now

--- clck.py
+++ clck.py
@@ -2,8 +2,6 @@
 import sys
 import argparse
 
-print("Why don't we just give this boy one more good old testaroo!")
-
 #list of args passed by terminal, [0] is filepath, start at [1]
 params = sys.argv
 
