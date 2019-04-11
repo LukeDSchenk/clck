b1e5b2a2cb010e09927842a9cb91fdd4ec55d95f
Luke Schenk <41804945+LukeDSchenk@users.noreply.github.com>
2019-04-11 14:53:30 -0400

    Add files via upload

---
+++ clck.py
@@ -0,0 +1,11 @@
+#/usr/bin/python3
+import sys
+import argparse
+
+print("This is a sample print difference.")
+
+#list of args passed by terminal, [0] is filepath, start at [1]
+params = sys.argv
+
+if __name__ == "__main__":
+    pass
---
+++ timesheet.py
@@ -0,0 +1,22 @@
+import csv
+from datetime import datetime
+
+class timesheet:
+    def __init__(self, name=""):
+        self.header = ["DATE", "START", "END", "HOURS", "NOTES"]
+        self.name = self.init_name(name)
+
+    def init_name(self, name):
+        dt = datetime.now()
+        if name == "":
+            name = dt.strftime("%d-%m-%Y--%H:%M:%S")
+        name = name + ".csv"
+
+        return name
+
+if __name__ == "__main__":
+    ts = timesheet("")
+    with open("./timesheets/" + ts.name, "w", encoding="utf-8", newline="") as new_ts:
+        writer = csv.writer(new_ts)
+
+        writer.writerow(ts.header)
