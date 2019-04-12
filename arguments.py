71c92252ca5674481fee2e8d3d16a5ecc7d6e587
LukeDSchenk <lschenk@next.tech>
2019-04-12 00:45:38 -0400

    update arguments.py

--- arguments.py
+++ arguments.py
@@ -1,19 +1,25 @@
 import argparse
+import sys
 
-def init_parser():
+def parse_args(list=sys.argv):
     welcome = """clck is a command line application that mimics a physical clock
      in system, designed for remote workers needing to track their own hours.
      The goal of this application is to make it as simple and intuitive for a general
      user (like a real clock in system!) while still providing some more in depth
      features for those who wish to use them."""
-    parser = argparse.ArgumentParser(description=welcome)
 
-    parser.add_argument('--test', '-i', help='starts the timer on the current timesheet (think "Clock in")')
+    parser = argparse.ArgumentParser(description=welcome)
+    parser.add_argument('--test', '-t', help='just an example argument')
+
+    subparsers = parser.add_subparsers(help='sub-command help')
+    in_parser = subparsers.add_parser('in', description='start the timer on the current timesheet',
+                                      help='in help')
+    out_parser = subparsers.add_parser('out',description='stop the timer on the current timesheet',
+                                       help='out help')
 
     args = parser.parse_args()
 
-    test = args.test
-    print(test)
+    print(args)
 
 if __name__ == "__main__":
-    init_parser()
+    parse_args()
